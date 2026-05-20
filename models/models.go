6391456df7a11e85847416e99c45e53a04ef17ba
DEELAGRA <deelagra@gmail.com>
2026-05-20 21:20:53 +0300

    Add config file

--- models/models.go
+++ models/models.go
@@ -1,18 +1,28 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
-type Departament struct {
-	ID         int
-	Name       string
-	Parent_id  int
-	Created_at time.Time
+type Department struct {
+	ID         int       `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name       string    `gorm:"not null" json:"name"`
+	Parent_id  int       `gorm:"index" json:"parent_id"`
+	Created_at time.Time `gorm:"autoCreateTime" json:"created_at"`
+}
+
+func (Department) TableName() string {
+	return "department"
 }
 
 type Employee struct {
-	ID             int
-	Departament_id *Departament
-	Full_name      string
-	Position       string
-	Hired_at       time.Time
+	ID             int         `json:"id"`
+	Departament_id *Department `json:"departament_id"`
+	Full_name      string      `json:"full_name"`
+	Position       string      `json:"position"`
+	Hired_at       time.Time   `json:"hired_at"`
+}
+
+func (Employee) TableName() string {
+	return "employee"
 }
